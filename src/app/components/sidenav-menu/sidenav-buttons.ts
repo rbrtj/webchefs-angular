23397bc5d3486a343329d0a0fce25f4122d0554c
rbrtj <robertjaszczurek@icloud.com>
2023-02-05 21:31:47 +0100

    Added sidenav menu with toggle button

---
+++ src/app/components/sidenav-menu/sidenav-buttons.ts
@@ -0,0 +1,9 @@
+export const SidenavButtons = [
+  {name: 'Statistics', imagePath: '../../../assets/statistics.svg'},
+  {name: 'Users', imagePath: '../../../assets/users.svg', submenus: [
+    {name: 'User List'},
+    {name: 'User Edit'},
+    {name: 'Roles and Permissions'},
+    {name: 'Settings'}
+  ]}
+]
---
+++ src/app/components/sidenav-menu/sidenav-menu.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { SidenavButtons } from './sidenav-buttons';
+@Component({
+  selector: 'app-sidenav-menu',
+  templateUrl: './sidenav-menu.component.html',
+  styleUrls: ['./sidenav-menu.component.scss']
+})
+export class SidenavMenuComponent implements OnInit {
+
+  sidenavButtons: any[] = [];
+  isExpanded: boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.sidenavButtons = SidenavButtons;
+  }
+
+}
